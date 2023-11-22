06585d0760f598766967867b9ebc73240b9da552
R0CAKIES <qrpgaabelar@tip.edu.ph>
2023-11-22 21:05:19 +0800

    Fixed other problems, and improved the look of the To-Buy to match the initial design.

--- lib/widgets/shared/miscellaneous/checkbox_tile.dart
+++ lib/widgets/shared/miscellaneous/checkbox_tile.dart
@@ -3,39 +3,40 @@ import "package:fridgital/shared/constants.dart";
 import "package:fridgital/widgets/shared/miscellaneous/round_check_box.dart";
 
 class CheckBox extends StatelessWidget {
-  CheckBox({required this.itemNameToBuy, required this.itemObtainStatus, required this.onChanged, super.key});
+  const CheckBox({required this.itemNameToBuy, required this.itemObtainStatus, required this.onChanged, super.key});
 
   final String itemNameToBuy;
   final bool itemObtainStatus;
   // ignore: avoid_positional_boolean_parameters
-  void Function(bool)? onChanged;
+  final void Function(bool)? onChanged;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: SizedBox(
-              width: 20,
-              height: 20,
-              child: RoundCheckBox(
-                color: TagColors.selectable[2],
-                value: itemObtainStatus,
-                onChanged: onChanged,
-              ),
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SizedBox(
+            width: 20,
+            height: 20,
+            child: RoundCheckBox(
+              color: TagColors.selectable[2],
+              value: itemObtainStatus,
+              onChanged: onChanged,
             ),
           ),
-          Text(
-            itemNameToBuy,
-            style: TextStyle(
-              fontSize: 20.0,
-              color: itemObtainStatus ? FigmaColors.lightGreyAccent : Colors.black,
-            ),
+        ),
+        Text(
+          itemNameToBuy,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: itemObtainStatus ? FigmaColors.lightGreyAccent : FigmaColors.textDark,
+            fontStyle: itemObtainStatus ? FontStyle.italic : FontStyle.normal,
+            decoration: itemObtainStatus ? TextDecoration.lineThrough : TextDecoration.none,
+            decorationColor: FigmaColors.lightGreyAccent,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
