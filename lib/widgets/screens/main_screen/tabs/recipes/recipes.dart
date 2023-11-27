ef1596e51da5f6782469f195deccc0e93fda0c56
R0CAKIES <qrpgaabelar@tip.edu.ph>
2023-11-28 00:01:40 +0800

    Started on the layout of Suggested Recipes.
    Co-authored-by: water-mizuu <water-mizuu@users.noreply.github.com>

--- lib/widgets/screens/main_screen/tabs/recipes/recipes.dart
+++ lib/widgets/screens/main_screen/tabs/recipes/recipes.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:fridgital/shared/constants.dart";
 import "package:fridgital/widgets/shared/miscellaneous/basic_screen.dart";
 import "package:mouse_scroll/mouse_scroll.dart";
 
@@ -9,11 +10,8 @@ class Recipes extends StatelessWidget {
   Widget build(BuildContext context) {
     return const BasicScreenWidget(
       child: MouseSingleChildScrollView(
-        child: Row(
-          children: [
-            RecipeTitle(),
-            SizedBox(height: 16.0),
-          ],
+        child: Column(
+          children: [RecipeTitle(), SizedBox(height: 16.0), RecipeTab()],
         ),
       ),
     );
@@ -28,7 +26,7 @@ class RecipeTitle extends StatelessWidget {
     var theme = Theme.of(context);
 
     return Padding(
-      padding: const EdgeInsets.all(32.0),
+      padding: const EdgeInsets.only(top: 32.0) + const EdgeInsets.symmetric(horizontal: 32.0),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -37,6 +35,57 @@ class RecipeTitle extends StatelessWidget {
           const SizedBox(height: 8.0),
           Text("Meals you can make based on your inventory.", style: theme.textTheme.displayLarge),
         ],
+      ),
+    );
+  }
+}
+
+class RecipeTab extends StatelessWidget {
+  const RecipeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32.0),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        child: ColoredBox(
+          color: FigmaColors.whiteAccent,
+          child: Row(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 16.0),
+                child: Text("hi"),
+              ),
+              SizedBox(
+                height: 200,
+                child: AspectRatio(
+                  aspectRatio: 1.0,
+                  child: Image.asset(
+                    "assets/images/pesto.jpg",
+                    width: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              // ShaderMask(
+              //   shaderCallback: (rect) {
+              //     return const LinearGradient(colors: [Colors.transparent, Colors.black])
+              //         .createShader(Offset.zero & rect.size);
+              //   },
+              //   blendMode: BlendMode.dstIn,
+              //   child: AspectRatio(
+              //     aspectRatio: 1.0,
+              //     child: Image.asset(
+              //       "assets/images/pesto.jpg",
+              //       width: 200,
+              //       fit: BoxFit.cover,
+              //     ),
+              //   ),
+              // ),
+            ],
+          ),
+        ),
       ),
     );
   }
