facf3dfb125ac5dfffb0ffb39d7b98bf982e201d
water-mizuu <mizu.github.em@gmail.com>
2023-11-30 22:49:47 +0800

    Added documentation to [useTagDataFuture]

--- lib/shared/hooks/use_tag_data_future.dart
+++ lib/shared/hooks/use_tag_data_future.dart
@@ -1,6 +1,7 @@
 import "package:flutter_hooks/flutter_hooks.dart";
 import "package:fridgital/back_end/tag_data.dart";
 
+/// Returns a [Future] that resolves to an empty [TagData] object.
 Future<TagData> useTagDataFuture() {
   late Future<TagData> tagDataFuture;
 
