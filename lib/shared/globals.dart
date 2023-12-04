83b0601f94cb0b8ec0a379406e5b930f2d63c703
water-mizuu <mizu.github.em@gmail.com>
2023-12-05 00:20:59 +0800

    Removed unused mechanics in [globals]

--- lib/shared/globals.dart
+++ lib/shared/globals.dart
@@ -11,42 +11,32 @@ import "package:sqflite_common_ffi/sqflite_ffi.dart";
 // SQLITE DATABASE
 // *****************************************
 
-late Database _database;
+late final Database database;
 
-Database get database => _database;
-
-@Deprecated("Do not use this setter.")
-set database(Database value) => _database = value;
-
-Future<Database>? _fetchedDatabase;
 Future<Database> fetchDatabase() async {
-  if (_fetchedDatabase case null) {
-    var options = OpenDatabaseOptions();
+  var options = OpenDatabaseOptions();
 
-    var isWeb = kIsWeb;
-    var isDesktop = !isWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS);
-    var isMobile = !isWeb && (Platform.isAndroid || Platform.isIOS || Platform.isFuchsia);
+  var isWeb = kIsWeb;
+  var isDesktop = !isWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS);
+  var isMobile = !isWeb && (Platform.isAndroid || Platform.isIOS || Platform.isFuchsia);
 
-    if (isDesktop) {
-      if (Platform.isWindows || Platform.isLinux) {
-        sqfliteFfiInit();
-      }
-
-      databaseFactory = databaseFactoryFfi;
-
-      var path = await getDatabasesPath();
-
-      _fetchedDatabase = databaseFactory.openDatabase(path, options: options);
-    } else if (isMobile) {
-      var databasePath = path.join(await getDatabasesPath(), "fridgital.db");
-
-      _fetchedDatabase = databaseFactory.openDatabase(databasePath, options: options);
-    } else {
-      _fetchedDatabase = databaseFactory.openDatabase(inMemoryDatabasePath, options: options);
+  if (isDesktop) {
+    if (Platform.isWindows || Platform.isLinux) {
+      sqfliteFfiInit();
     }
-  }
 
-  return _fetchedDatabase!;
+    databaseFactory = databaseFactoryFfi;
+
+    var path = await getDatabasesPath();
+
+    return await databaseFactory.openDatabase(path, options: options);
+  } else if (isMobile) {
+    var databasePath = path.join(await getDatabasesPath(), "fridgital.db");
+
+    return await databaseFactory.openDatabase(databasePath, options: options);
+  } else {
+    return await databaseFactory.openDatabase(inMemoryDatabasePath, options: options);
+  }
 }
 
 // *****************************************
