babd9b12dbf4c15eb850c511dc1accba55f1d8d2
water-mizuu <mizu.github.em@gmail.com>
2023-12-04 14:51:37 +0800

    Fixed `useTagDataFuture` on spawning instances

--- lib/shared/hooks/use_tag_data_future.dart
+++ lib/shared/hooks/use_tag_data_future.dart
@@ -1,17 +1,13 @@
+import "dart:async";
+
 import "package:flutter_hooks/flutter_hooks.dart";
 import "package:fridgital/back_end/change_notifiers.dart/tag_data.dart";
 
 /// Returns a [Future] that resolves to an empty [TagData] object.
 Future<TagData> useTagDataFuture() {
-  late Future<TagData> tagDataFuture;
+  var future = useMemoized(TagData.emptyFromDatabase);
 
-  useEffect(() {
-    tagDataFuture = TagData.emptyFromDatabase();
+  useEffect(() => () => unawaited(future.then((t) => t.dispose())));
 
-    return () {
-      tagDataFuture.then((tagData) => tagData.dispose());
-    };
-  });
-
-  return tagDataFuture;
+  return future;
 }
